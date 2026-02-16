122e4a4cd71d2cc7aa1bc75f78a78621b837c475
Veronika Timofeeva <veronika.t.2801@mail.ru>
2026-02-16 21:11:37 +0700

    14

---
+++ 14.py
@@ -0,0 +1,26 @@
+hint = input('Введите подсказку:')
+word = input('Загаданное слово:')
+print('\n' * 25)
+print(hint)
+guessed_word = ['*'] * len(word)
+print(''.join(guessed_word))
+attempts_count = 0
+while attempts_count <= 10:
+    choice = input("Буква или слово (0 - буква, 1 - слово)?")
+    if choice == '0':
+        letter = input()
+        for ch in range(len(word)):
+            if word[ch] == letter:
+               guessed_word[ch] = letter
+        print(''.join(guessed_word))
+        if '*' not in guessed_word:
+            print('Победа!')
+            break
+    elif choice == '1':
+        entered_word = input()
+        if entered_word == word:
+            print('Победа!')
+        else:
+            print('Проигрыш :(')
+        break
+    attempts_count += 1
